1871d3a540c651aacb4a81363ea4ad0cb1798047
isralr <ilaramac@myuax.com>
2024-12-08 14:54:52 +0100

    ejercicio 6 bien hecho

--- matematicas/flujo_cadenas.cpp
+++ matematicas/flujo_cadenas.cpp
@@ -1,5 +1,15 @@
-//
-// Created by israe on 08/12/2024.
-//
+#include <iostream>
+#include <sstream>
 
-#include "flujo_cadenas.h"
+void ejecutarEjercicioFlujoCadenas() {
+    std::string text;
+    std::cout << "Ingresa un texto: ";
+    std::getline(std::cin, text);
+
+    std::stringstream ss;
+    ss << text;
+
+    std::string text_from_ss;
+    ss >> text_from_ss;
+    std::cout << "El texto desde el flujo de cadena es: " << text_from_ss << std::endl;
+}
