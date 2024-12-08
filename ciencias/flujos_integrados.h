faeafd5fa6f208c5a0e6bce7884f3d075695366e
isralr <ilaramac@myuax.com>
2024-12-08 14:53:06 +0100

    ejercicio 2 bien hecho

--- ciencias/flujos_integrados.h
+++ ciencias/flujos_integrados.h
@@ -1,16 +1,6 @@
-//
-// Created by israe on 08/12/2024.
-//
-
 #ifndef FLUJOS_INTEGRADOS_H
 #define FLUJOS_INTEGRADOS_H
 
+void ejecutarEjercicioFlujosIntegrados();
 
-
-class flujos_integrados {
-
-};
-
-
-
-#endif //FLUJOS_INTEGRADOS_H
+#endif
