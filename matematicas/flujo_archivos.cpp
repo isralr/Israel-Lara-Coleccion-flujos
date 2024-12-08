a1f8d1b994292293c36ebc33e1c30f34a111917c
isralr <ilaramac@myuax.com>
2024-12-08 14:54:33 +0100

    ejercicio 5 bien hecho

--- matematicas/flujo_archivos.cpp
+++ matematicas/flujo_archivos.cpp
@@ -1,5 +1,18 @@
-//
-// Created by israe on 08/12/2024.
-//
+#include <iostream>
+#include <fstream>
 
-#include "flujo_archivos.h"
+void ejecutarEjercicioFlujoArchivos() {
+    std::string text;
+    std::cout << "Ingresa un texto: ";
+    std::cin.ignore(); // Limpia el buffer
+    std::getline(std::cin, text);
+
+    std::ofstream file("example.txt");
+    if (file) {
+        file << text;
+        std::cout << "El texto se guardó correctamente.\n";
+    } else {
+        std::cerr << "No se pudo abrir el archivo.\n";
+    }
+    file.close();
+}
