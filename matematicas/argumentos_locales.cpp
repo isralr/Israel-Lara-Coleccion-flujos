db5ad10dca3b293e6c0105607b8cf475b61584eb
isralr <ilaramac@myuax.com>
2024-12-08 14:56:25 +0100

    ejercicio 7 bien hecho

--- matematicas/argumentos_locales.cpp
+++ matematicas/argumentos_locales.cpp
@@ -1,5 +1,18 @@
-//
-// Created by israe on 08/12/2024.
-//
+#include <iostream>
+#include <fstream>
 
-#include "argumentos_locales.h"
+void ejecutarEjercicioArgumentosLocales(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Por favor, proporciona un argumento de texto.\n";
+        return;
+    }
+
+    std::ofstream file("example.txt");
+    if (file) {
+        file << argv[1];
+        std::cout << "El texto se guardó correctamente.\n";
+    } else {
+        std::cerr << "No se pudo abrir el archivo.\n";
+    }
+    file.close();
+}
